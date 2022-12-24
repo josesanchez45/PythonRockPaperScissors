32fc99f8c657da416f0455427f8c98a408278048
jose sanchez <jose.sanchez.artist@gmail.com>
2022-12-23 16:04:10 -0800

    cleaned up UI

--- main.py
+++ main.py
@@ -31,13 +31,15 @@ rockPaperScissors = [rock, paper, scissors]
 choice = input("Choose an option: 0 for rock, 1 for paper and 2 for scissors\n")
 userChoice= int(choice)
 computerChoice = random.randint(0,2)
+if(userChoice != 1 or userChoice != 0 or userChoice != 2):
+    print("Sorry not a valid number")
 if userChoice == computerChoice:
-    print(f"{rockPaperScissors[userChoice]}\n{rockPaperScissors[computerChoice]}\n Its a Draw")
+    print(f"Your Choice: {rockPaperScissors[userChoice]}\n Computer chooses: {rockPaperScissors[computerChoice]}\n Its a Draw")
 elif userChoice == 0 and computerChoice == 1:
-    print(f"{rockPaperScissors[userChoice]}\n{rockPaperScissors[computerChoice]}\n You Lose")
+    print(f"Your Choice:{rockPaperScissors[userChoice]}\nComputer chooses: {rockPaperScissors[computerChoice]}\n You Lose")
 elif userChoice == 1 and computerChoice == 2:
-    print(f"{rockPaperScissors[userChoice]} \n{rockPaperScissors[computerChoice]}\n You Lose")
+    print(f"Your Choice:{rockPaperScissors[userChoice]} \nComputer chooses: {rockPaperScissors[computerChoice]}\n You Lose")
 elif userChoice == 2 and computerChoice == 0:
-    print(f"{rockPaperScissors[userChoice]}{rockPaperScissors[computerChoice]} You Lose")
+    print(f"Your Choice:{rockPaperScissors[userChoice]}Computer chooses: {rockPaperScissors[computerChoice]} You Lose")
 else:
-    print(f"{rockPaperScissors[userChoice]}{rockPaperScissors[computerChoice]} You Win")
+    print(f"Your Choice:{rockPaperScissors[userChoice]}Computer chooses: {rockPaperScissors[computerChoice]} You Win")
